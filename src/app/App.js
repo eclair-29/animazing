af7a24add16c08d0016cfc7858dc3d392f458fe1
Migz De Chavez <dmgzky@gmail.com>
2019-03-04 22:25:22 +0800

    test commit

--- src/app/App.js
+++ src/app/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class App extends Component {
   render() {
-    return <div className="App">{/* ADD CONTENTS HERE... */}</div>;
+    return <div className="App">{/* ADD CONTENTS HERE AND UPDATE... */}</div>;
   }
 }
 
